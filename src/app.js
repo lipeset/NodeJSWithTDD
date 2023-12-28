34c014687555292ac3fc03a759df419dc6bb64c3
Felipe Lucas <felipea.lucas@outlook.com>
2023-12-28 00:54:57 -0300

    add nova rota ao arquivo

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ app.get("/", (req, res) => {
 	res.status(200).send();
 });
 
+app.get("/users", (req, res) => {
+	const users = [
+		{name: "Felipe Lucas", email: "felipe@email.com"}
+	];
+	res.status(200).json(users);
+});
+
 module.exports = app;
