d7ba239bd91dd70c3fafdfeab8f5dd48d4e43b5d
Felipe Lucas <felipea.lucas@outlook.com>
2023-12-28 00:26:27 -0300

    arquivo não mais necessário

--- test/server.test.js
+++
@@ -1,9 +0,0 @@
-const supertest = require("supertest");
-
-const request = supertest("http://localhost:3001");
-
-test("Deve responder na porta 3001", () => {
-	//acessar a URL http://localhost:3001
-	//retorno esperado: 200 (OK)
-	return request.get("/").then(res => expect(res.status).toBe(200));
-});
