31902d9152be227d67a90853cd7051367d15d2b8
Felipe Lucas <felipea.lucas@outlook.com>
2023-12-30 18:42:57 -0300

    arquivo renomeado

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,20 +1,21 @@
 const request = require("supertest");
 
-const app = require("../src/app");
+const app = require("../../src/app");
 
 test("Deve listar todos os usuários", () =>{
 	return request(app).get("/users").then(res => {
 		expect(res.status).toBe(200);
-		expect(res.body).toHaveLength(1);
-		expect(res.body[0]).toHaveProperty("name", "Felipe Lucas");
+		expect(res.body.length).toBeGreaterThan(0);
 	});
 });
 
 test("Deve inserir um usuário com sucesso", () => {
+	const email = `${Date.now()}@email.com`;
 	return request(app)
 		.post("/users").send({
 			name: "Felipe Almeida",
-			email: "felipe2@email.com"
+			email,
+			password: "1337"
 		}).then(res => {
 			expect(res.status).toBe(201);
 			expect(res.body.name).toBe("Felipe Almeida");
