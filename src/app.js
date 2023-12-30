229b9abb0c104effc0a2fa2f11efcbfa0d30037f
Felipe Lucas <felipea.lucas@outlook.com>
2023-12-30 20:10:00 -0300

    adição do KNEXLOGGER

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const app = require("express")();
 const consign = require("consign");
 const knex = require("knex");
 const knexfile = require("../knexfile");
+//ACONSELHAVEL DEIXAR DESLIGADO PARA NÃO SUJAR O CONSOLE
+//HABILITAR QUANDO SE QUER ALGUMA INFO DE ERRO
+//const knexlogger = require("knex-logger"); //LOG COM O KNEXLOGGER
 
 // TODO criar chaveamento dinâmico
 app.db = knex(knexfile.test);
+
+//ACONSELHAVEL DEIXAR DESLIGADO PARA NÃO SUJAR O CONSOLE
+//HABILITAR QUANDO SE QUER ALGUMA INFO DE ERRO
+//app.use(knexlogger(app.db)); //LOG COM O KNEXLOGGER
 
 consign({cwd: "src", verbose: false})
 	.include("./configs/middlewares.js")
@@ -17,5 +24,11 @@ app.get("/", (req, res) => {
 });
 
 
+// LOG COM O CONSOLE.LOG - MENOS PRÁTICO E MAIS LENTO
+// app.db.on("query", (query) => {
+// 	console.log({sql: query.sql, bindings: query.bindings ? query.bindings.join(",") : ""})
+// 		.on("query-response", response => console.log(response))
+// 		.on("error", error => console.log(error));
+// });
 
 module.exports = app;
