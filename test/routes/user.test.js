1fd13a07681350317595d512cc93cc0047abd207
Felipe Lucas <felipea.lucas@outlook.com>
2024-01-01 21:34:28 -0300

    criando teste de email repetido e refatorando para aplicar essa lógica

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 
 const app = require("../../src/app");
 
+const email = `${Date.now()}@email.com`;
+
 test("Deve listar todos os usuários", () =>{
 	return request(app).get("/users").then(res => {
 		expect(res.status).toBe(200);
@@ -10,7 +12,6 @@ test("Deve listar todos os usuários", () =>{
 });
 
 test("Deve inserir um usuário com sucesso", () => {
-	const email = `${Date.now()}@email.com`;
 	return request(app)
 		.post("/users")
 		.send({
@@ -61,4 +62,17 @@ test("Não deve inserir usuário sem senha", (done) => {
 			expect(res.body.error).toBe("Senha é um atributo obrigatório");
 			done();
 		}).catch(err => done.fail(err));
+});
+
+test("Não deve inserir usuário com email existente", () => {
+	return request(app).post("/users")
+		.send({
+			name: "Felipe Almeida",
+			email,
+			password: "1337"
+		})
+		.then(res => {
+			expect(res.status).toBe(400);
+			expect(res.body.error).toBe("Já existe um usuário com este email");
+		});
 });
