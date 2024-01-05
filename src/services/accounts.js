840c8ccd232a3337a5ea7a6e6f869b6e4c86dc00
Felipe Lucas <felipea.lucas@outlook.com>
2024-01-05 16:15:44 -0300

    adição do novo método UPDATE e seu respectivo teste

--- src/services/accounts.js
+++ src/services/accounts.js
@@ -1,15 +1,24 @@
 module.exports = (app) => {
 	const findAll = () => {
-		return app.db("accounts").select();
+		return app.db("accounts")
+			.select();
 	};
 
 	const save = async (account) => {
-		return app.db("accounts").insert(account, "*");
+		return app.db("accounts")
+			.insert(account, "*");
 	};
 
 	const find = (filter = {}) => {
-		return app.db("accounts").where(filter).first();
+		return app.db("accounts")
+			.where(filter).first();
+	};
+
+	const update = (id, account) => {
+		return app.db("accounts")
+			.where({id})
+			.update(account, "*");
 	};
 	
-	return {findAll, save, find};
+	return {findAll, save, find, update};
 };
